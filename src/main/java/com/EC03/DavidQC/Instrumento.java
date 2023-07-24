cc676462e57063c8209fab2dd8d3c6d9313356f4
DavidCaldas934 <134322937+DavidCaldas934@users.noreply.github.com>
2023-07-24 19:49:58 +0000

    Agregando Categoria

--- src/main/java/com/EC03/DavidQC/Instrumento.java
+++ src/main/java/com/EC03/DavidQC/Instrumento.java
@@ -1,19 +1,27 @@
 package com.EC03.DavidQC;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity 
 @Table(name = "instrumento")
 public class Instrumento {
+
     @Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
 	private String nombre;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_categoria")
+    private Categoria categoria;
 
     public Integer getId() {
         return id;
@@ -30,7 +38,14 @@ public class Instrumento {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
-    
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+ 
 }
 
--- src/main/java/com/EC03/DavidQC/InstrumentoRepository.java
+++ src/main/java/com/EC03/DavidQC/InstrumentoRepository.java
@@ -1,7 +1,15 @@
 package com.EC03.DavidQC;
 
-import org.springframework.data.repository.CrudRepository;
 
-public interface InstrumentoRepository extends CrudRepository<Instrumento, Integer>{
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "instrumentos", path = "instrumentos")
+public interface InstrumentoRepository extends PagingAndSortingRepository<Instrumento, Integer>, CrudRepository<Instrumento, Integer>{
     
+    List<Instrumento> findByNombre(@Param("nombre") String nombre);    
 }
